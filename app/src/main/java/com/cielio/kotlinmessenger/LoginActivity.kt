5728247611f21bf8cd50cbc62553607bdc32c1f0
cielio <cielion@hotmail.com>
2021-03-25 00:22:08 -0300

    added signInWithEmailAndPassword FireBase

--- app/src/main/java/com/cielio/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/cielio/kotlinmessenger/LoginActivity.kt
@@ -2,8 +2,9 @@ package com.cielio.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.cielio.kotlinmessenger.databinding.ActivityLoginBinding
-import com.cielio.kotlinmessenger.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -12,5 +13,21 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEdittextLogin.text.toString()
+            val password = binding.passwordEdittextLogin.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful){
+                        Toast.makeText(this,"Logado", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+        binding.backToRegisterTextview.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/cielio/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/cielio/kotlinmessenger/MainActivity.kt
@@ -3,7 +3,14 @@ package com.cielio.kotlinmessenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
 import com.cielio.kotlinmessenger.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,13 +20,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.registerButtonRegister.setOnClickListener {
-            val email = binding.emailEdittextRegister.text.toString()
-            val password = binding.passwordEdittextRegister.text.toString()
+        binding.registerButtonLogin.setOnClickListener {
+            performRegister()
         }
+
         binding.alreadyHaveAccountView.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val email = binding.emailEdittextLogin.text.toString()
+        val password = binding.passwordEdittextLogin.text.toString()
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Por favor digite o Email ou senha.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Main", "Succesfully created User with uid ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                Log.d("Main","Failed to create user: ${it.message}")
+            }
     }
 }
